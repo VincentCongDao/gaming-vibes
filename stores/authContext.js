246d077d05300e16cf08c53799d67bec268aabd4
Vincet CD <congdanhdao95@gmail.com>
2021-05-25 21:20:44 -0700

    change some stuff

--- stores/authContext.js
+++ stores/authContext.js
@@ -1,26 +1,31 @@
-import { createContext, useState, useEffect } from 'react'
-import netlifyIdentity from 'netlify-identity-widget'
+import { createContext, useState, useEffect } from "react";
+import netlifyIdentity from "netlify-identity-widget";
 
 const AuthContext = createContext({
   user: null,
   login: () => {},
   logout: () => {},
-  authReady: false
-})
+  authReady: false,
+});
 
 export const AuthContextProvider = ({ children }) => {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     // init netlify identity connection
-    netlifyIdentity.init()
-  }, [])
+    // Whether if the user is login or logout
+    netlifyIdentity.init();
+  }, []);
 
+  // When user click on the login button to activate this function
+  const login = () => {
+    netlifyIdentity.open();
+  };
+
+  const context = { user, login };
   return (
-    <AuthContext.Provider value={user}>
-      { children }
-    </AuthContext.Provider>
-  )
-}
+    <AuthContext.Provider value={context}>{children}</AuthContext.Provider>
+  );
+};
 
-export default AuthContext
+export default AuthContext;
