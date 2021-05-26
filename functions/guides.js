ed6d339acbce5a788445801521b0778dbbc98732
Vincet CD <congdanhdao95@gmail.com>
2021-05-26 00:40:48 -0700

    serverless update

--- functions/guides.js
+++ functions/guides.js
@@ -4,8 +4,15 @@ exports.handler = async (event, context) => {
     { title: "Random Event In These Games", author: "CupTea" },
     { title: "Random Event In These Games", author: "CupTea" },
   ];
+
+  if (context.clientContext.user) {
+    return {
+      statuscode: 200,
+      body: JSON.stringify(guides),
+    };
+  }
   return {
-    statuscode: 200,
-    body: JSON.stringify(guides),
+    statuscode: 401,
+    body: JSON.stringify({ message: "Not Login" }),
   };
 };
--- pages/guides.js
+++ pages/guides.js
@@ -1,11 +1,20 @@
+import { useContext, useEffect } from "react";
 import styles from "../styles/Guides.module.css";
-
+import AuthContext from "../stores/authContext";
 export default function Guides() {
+  const { user, authReady } = useContext(AuthContext);
   useEffect(() => {
-    fetch("/.netlify/functions/guides")
-      .then((res) => res.json())
-      .then((data) => console.log(data));
-  }, []);
+    if (authReady) {
+      fetch(
+        "/.netlify/functions/guides",
+        user && {
+          headers: { Authorization: "Bearer" + user.token.access_token },
+        }
+      )
+        .then((res) => res.json())
+        .then((data) => console.log(data));
+    }
+  }, [user]);
   return (
     <div className={styles.guides}>
       <h2>All Guides</h2>
