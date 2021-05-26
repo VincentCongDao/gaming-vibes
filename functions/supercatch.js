63da8c897caaae64dcf4de96c6e03a258902047d
Vincet CD <congdanhdao95@gmail.com>
2021-05-25 22:27:49 -0700

    update functions

--- functions/supercatch.js
+++ functions/supercatch.js
@@ -2,10 +2,10 @@ exports.handler = async () => {
   console.log("function ran");
 
   const data = { name: "Vincent", age: 25, job: "something" };
-};
 
-// Return respnse to browser session
-return {
-  statuscode: 200,
-  body: JSON.stringify(data),
+  // Return response to browser session
+  return {
+    statuscode: 200,
+    body: JSON.stringify(data),
+  };
 };
